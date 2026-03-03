6b2ef8ace33dee4e98a7d40e0579fa6d6edcfa78
Ilyas Garipov <main.ilyasgaripov@gmail.com>
2026-03-03 21:16:49 +0300

    listen and serve

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("go http server")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "hello, go!")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
