848dc8798ae41ca837903d97700e3c6f107d1b01
Ilyas Garipov <main.ilyasgaripov@gmail.com>
2026-03-03 22:38:01 +0300

    routing

--- main.go
+++ main.go
@@ -11,17 +11,29 @@ func main() {
 
 	fmt.Println("Listening on 127.0.0.1:8080")
 
+	routes := map[string]func(w http.ResponseWriter, r *http.Request){
+		"/dog": func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("%s %s\n", r.Method, r.URL.Path)
+			io.WriteString(w, "dog")
+		},
+		"/cat": func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("%s %s\n", r.Method, r.URL.Path)
+			io.WriteString(w, "cat")
+		},
+	}
+
 	indexHandler := func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
+		if _, ok := routes[r.URL.Path]; !ok {
+			log.Printf("%s %s page not found\n", r.Method, r.URL.Path)
 			http.NotFound(w, r)
 			return
 		}
-
-		log.Println("INFO: ", "GET /")
-		io.WriteString(w, "hello, go!")
 	}
-
 	http.HandleFunc("/", indexHandler)
+
+	for route := range routes {
+		http.HandleFunc(route, routes[route])
+	}
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
